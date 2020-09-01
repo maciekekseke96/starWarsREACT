f11d187f6b774607ac4ad563a5383cfbb1ddeb22
Maciej <maciek20145c@wp.pl>
2020-09-01 12:53:48 +0200

    Initial commit for Star Wars project

---
+++ README.md
@@ -0,0 +1,2 @@
+Tic Tac Toe.
+This project includes one of the most popular games in the world which has been created in REACT.js.
---
+++ src/Components/App/App.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+
+function App() {
+  return <h1>Hello, it's fresh React project</h1>;
+}
+
+export default App;

153fbf013c65538fede5977db0e26d86e3dbb7a9
Maciej Mandzak <maciekekseke@gmail.com>
2020-09-02 12:37:03 +0200

    Loading screen for tables prepared

--- src/Components/App/MainContent/MovieElement/TableElement/TableElement.js
+++ src/Components/App/MainContent/MovieElement/TableElement/TableElement.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import LoadingScreen from "./LoadingScreen/LoadingScreen";
 import "./TableElement.scss";
 
 const TableElement = (props) => {
@@ -17,50 +18,54 @@ const TableElement = (props) => {
     });
   }, []);
 
-  return (
-    <div className="tableElement">
-      <table>
-        <thead>
-          <tr>
-            <th>
-              Planet
-              <br /> Name
-            </th>
-            <th>
-              Rotation
-              <br /> Period
-            </th>
-            <th>
-              Orbital
-              <br /> Period
-            </th>
-            <th>Diameter</th>
-            <th>Climate</th>
-            <th>
-              Surface
-              <br /> Water
-            </th>
-            <th>Population</th>
-          </tr>
-        </thead>
-        <tbody>
-          {planets.map((planet, index) => {
-            return (
-              <tr key={index}>
-                <td>{planet.name}</td>
-                <td>{planet.rotation_period}</td>
-                <td>{planet.orbital_period}</td>
-                <td>{planet.diameter}</td>
-                <td>{planet.climate}</td>
-                <td>{planet.surface_water}</td>
-                <td>{planet.population}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+  if (planets.length == planetsAPIs.length) {
+    return (
+      <div className="tableElement">
+        <table>
+          <thead>
+            <tr>
+              <th>
+                Planet
+                <br /> Name
+              </th>
+              <th>
+                Rotation
+                <br /> Period
+              </th>
+              <th>
+                Orbital
+                <br /> Period
+              </th>
+              <th>Diameter</th>
+              <th>Climate</th>
+              <th>
+                Surface
+                <br /> Water
+              </th>
+              <th>Population</th>
+            </tr>
+          </thead>
+          <tbody>
+            {planets.map((planet, index) => {
+              return (
+                <tr key={index}>
+                  <td>{planet.name}</td>
+                  <td>{planet.rotation_period}</td>
+                  <td>{planet.orbital_period}</td>
+                  <td>{planet.diameter}</td>
+                  <td>{planet.climate}</td>
+                  <td>{planet.surface_water}</td>
+                  <td>{planet.population}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  } else {
+    return <LoadingScreen />;
+  }
 };
 
 export default TableElement;

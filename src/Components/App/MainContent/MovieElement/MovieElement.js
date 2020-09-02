44fc150a1537a3f9b6310c13f6c53f827838c71e
Maciej Mandzak <maciekekseke@gmail.com>
2020-09-02 11:18:17 +0200

    Logic of compasible elements prepared

--- src/Components/App/MainContent/MovieElement/MovieElement.js
+++ src/Components/App/MainContent/MovieElement/MovieElement.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
+import TableElement from "./TableElement/TableElement";
 import "./MovieElement.scss";
 
 const MovieElement = (props) => {
   const [isShown, setIsShown] = useState(false);
 
   return (
-    <div className="movieElement">
-      <p className="movieTitle">{props.movie.title}</p>
-      <div className="wrapBtn" onClick={()=>setIsShown(!isShown)}></div>
-    </div>
+    <>
+      <div className="movieElement">
+        <p className="movieTitle">{props.movie.title}</p>
+        <div className="wrapBtn" onClick={() => setIsShown(!isShown)}></div>
+      </div>
+      {isShown && <TableElement />}
+    </>
   );
 };
 
---
+++ src/Components/App/MainContent/MovieElement/TableElement/TableElement.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "./TableElement.scss";
+
+const TableElement = (props) => {
+  return (
+    <div className="tableElement"></div>
+  );
+};
+
+export default TableElement;
